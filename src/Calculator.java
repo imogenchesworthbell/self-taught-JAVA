7e1e94c7b8634af7a32714b5fb635e3c46eb8b63
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-11-06 10:22:36 +0000

    calculator improved

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
+// Calculator that allows addition, subtraction, multiplication, division and exponential.
+
 public class Calculator {
 
     public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to my Calculator");
 
-        double value1 = 100.0d;
-        double value2 = 50.0d;
-        double result = 0.0d;
-        char opCode = 'd';
+        System.out.println("Enter first value: ");
+        double value1 = scanner.nextDouble();
+
+        System.out.println("Enter second value: ");
+        double value2 = scanner.nextDouble();
+
+        System.out.println("Enter operation (a for addition, s for subtraction, m for multiplication, d for division, e for exponential): ");
+        char opCode = scanner.next().toLowerCase().charAt(0);
+
+        double result;
+
 
         switch (opCode){
             case 'a':
@@ -26,29 +38,14 @@ public class Calculator {
                     return;
                 }
                 break;
+            case 'e':
+                result = Math.pow(value1, value2);
+                break;
             default:
                 System.out.println("Invalid opCode: " + opCode);
                 result = 0.0d;
             break;
         }
         System.out.println("Result: " + result);
-    /*   //using if else instead of switch
-     if(opCode == 'a')
-            result = value1 + value2;
-        else if(opCode == 's')
-            result = value1 - value2;
-        else if(opCode == 'm')
-            result = value1 * value2;
-        else if(opCode == 'd'){
-            if(value2 != 0)
-                result = value1 / value2;
-
-        }
-        else {
-            System.out.println("Invalid opCode: " + opCode);
-            result = 0.0d;
-        }
-
-        System.out.println("Result: " + result);*/
     }
 }
