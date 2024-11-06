9a3e6a21c7b3a44318581502f8a454d01a78d132
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-11-06 11:44:57 +0000

    RockPaperScissors Working

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -5,7 +5,10 @@ public class MathUtils {
         }
         return (double) likes/(likes+dislikes)*100;
     }
-    public static double random(int x){
+    public static int random(int x){
          return (int)(Math.random() * (x+1));  // 0 to x
+        /* Another way - using math.random() and math.round():
+        return (int) Math.round(Math.random() * x)
+         */
     }
 }
--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,18 +1,34 @@
 public class RockPaperScissors {
 
-    public static void randomChoice() {
-        double choice = MathUtils.random(2);
+    public static String randomChoice() {
+        int choice = MathUtils.random(2);
 
         if (choice == 0) {
-            System.out.println("Rock");
+            return "Rock";
         } else if (choice == 1) {
-            System.out.println("Paper");
+            return "Paper";
         } else {
-            System.out.println("Scissors");
+            return "Scissors";
         }
     }
 
+    public static void winner(){
+        String player1 = randomChoice();
+        String player2 = randomChoice();
+
+        System.out.println("Player 1 chose: " + player1 );
+        System.out.println("Player 2 chose: " + player2 );
+
+        if ( player1.equals(player2)){
+            System.out.println("It's a tie");
+        } else if ((player1.equals("Rock") && player2.equals("Scissors")) || (player1.equals("Paper") && player2.equals("Rock")) || (player1.equals("Scissors") && player2.equals("Paper"))) {
+            System.out.println("Player 1 wins!");
+        } else {
+            System.out.println("Player 2 wins!");
+    }
+    }
+
     public static void main(String[] args){
-        randomChoice();
+        winner();
     }
 }
